71ee8cac2139d5bad6423826864369c3c0ea4c9f
Blokewed <peeter.sture@tktk.ee>
2024-12-17 22:57:09 +0200

    Initial commit

---
+++ Function/Program.cs
@@ -0,0 +1,24 @@
+﻿static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("Śee you later, alligator!");
+}
+
+
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+
+else
+{
+    PrintGoodbye();
+}
+
---
+++ Week7_Sture/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Week7_Sture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valige teisendus (C/F või F/C):");
+            string choice = Console.ReadLine().ToUpper();
+            if (choice == "C/F")
+            {
+                Console.WriteLine("Sisestage temperatuur Celsiuse järgi:");
+                double celsius = double.Parse(Console.ReadLine());
+                ConvertToFahrenheit(celsius);
+            }
+            else if (choice == "F/C")
+            {
+                Console.WriteLine("Sisestage temperatuur Fahrenheiti järgi:");
+                double fahrenheit = double.Parse(Console.ReadLine());
+                ConvertToCelsius(fahrenheit);
+            }
+            else
+            {
+                Console.WriteLine("Vigane valik");
+            }
+        }
+        static void ConvertToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine($"{fahrenheit} °F on {celsius} °C.");
+        }
+        static void ConvertToFahrenheit(double celsius)
+        {
+            double fahrenheit = celsius * 9 / 5 + 32;
+            Console.WriteLine($"{celsius} °C on {fahrenheit} °F.");
+        }
+    }
+}
